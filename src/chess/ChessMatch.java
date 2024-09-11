8f8728c555c3b3ba42b2692903ac51470ad59d6f
Ricardo Cesar <ol.rcesar@gmail.com>
2024-09-11 19:24:23 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,15 +29,19 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
-				
 	}
 	
 	private Piece makeMove(Position source, Position target) {
@@ -83,4 +87,6 @@ public class ChessMatch {
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
 		
 	}
+
+
 }
